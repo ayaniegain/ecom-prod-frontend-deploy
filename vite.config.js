a25c08f691e44bb0648373ba1db2e21ff764a661
ayaniegain <ayaniegain@gmail.com>
2024-06-09 00:45:47 +0530

    update change

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://ecom-prod-backend-deploy.onrender.com',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
