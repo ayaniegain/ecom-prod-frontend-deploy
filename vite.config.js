647096c0c5a7926c3cd86340916da1b85332d016
ayaniegain <ayaniegain@gmail.com>
2024-06-09 01:12:35 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    reactRefresh(), // Plugin for hot module replacement (HMR) during development
+    react(), // Plugin for handling React components and JSX
+  ],
   server: {
     proxy: {
       '/api': {
@@ -13,4 +18,15 @@ export default defineConfig({
       },
     },
   },
-})
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
